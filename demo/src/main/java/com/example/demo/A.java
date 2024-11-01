c8f6a661454ac0dd0ea0f3d709d143e0a9bcbb82
CharlieJ <zwj_0122@163.com>
2024-11-01 09:58:29 +0800

    666

--- demo/src/main/java/com/example/demo/A.java
+++ demo/src/main/java/com/example/demo/A.java
@@ -44,41 +44,88 @@ public class A  {
     // “非连续”的子序列 ，所以判断条件稍微复杂一点
 
 
-    public static int  searchInsert(int[] nums, int target) {
-
-        int left = 0 , right = nums.length-1;
-
-        //因为是 “左闭右闭” 原则 , 所以是  带等号的 <=
-
-        while(left <= right){
+    //
 
 
-            int mid = (left + right)/2;
-            if(nums[mid]>target)
-                //  high 下标要更新成  mid-1 了
-                right = mid - 1;
-            else if (nums[mid]<target)
-                // low 下标 要更新成 mid +1 了
-                left = mid + 1;
-            else
-                return mid;
+    private static List<String> result = new ArrayList<>(); // 记录结果
+
+    public static List<String> restoreIpAddresses(String s) {
+
+        // 这里的  startIndex 就是切割线，初始化是0
+        // 比如说 "11123" ，startIndex=0 的话，就是 "1|1123"
+
+        // 这里还要传入一个 pointNum 参数，代表往原始字符串s 添加的 点号. 数量，主要作为递归出口的判断
+        backtracking(s, 0, 0);
+
+        return result;
+
+    }
+
+    public static void backtracking(String s, int startIndex, int pointNum) {
+
+        // 点号 数量为 3 时，分隔结束，递归出口。说明已经分成了四个字串了，已经是 ip地址格式
+        if (pointNum == 3) {
+
+            // 只需要再判断 第四段，剩下的子串是否合法，如果合法就放进 result 中
+            if (isValid(s, startIndex, s.length() - 1)) {
+                // 这里的话比较特殊，因为String 类型是不可变的，所以这里直接加 s就可以，
+                result.add(s);
+            }
+            return;
+        }
+
+
+        for (int i = startIndex; i < s.length(); i++) {
+
+            // 通过 [startIndex,i] 这个区间，就能获取到 s的子串，然后判断 是否合法
+            if (isValid(s, startIndex, i)) {
+
+                // 注意！！！这里的话，对于符合 需要对 原始字符串 s 每次都更新 ！！添加 点号.  	                                和之前的那种temp.add()不太一样 ！！！！
+
+                //在原始字符串s 的i下标对应 的后面，插入一个逗点，主要通过substring(),注入传入的下标！！
+                // 其实还是很好理解的，先要截取 i之前的 所有字符串s ，然后加上 点号.  然后再拼接上 i 下标到剩下的 length()-1 的截取字符串s
+                s = s.substring(0, i + 1) + "." + s.substring(i + 1);
+                pointNum++;
+
+                backtracking(s, i + 2, pointNum); //插入点号. 后 ！！因为原始字符串s 多了个字符串，所以下一个子串的起始位置为 i + 2 。。这点也很特殊 ！！
+                pointNum--;         // 回溯，点号数量 -1
+                s = s.substring(0, i + 1) + s.substring(i + 2);    // 回溯删掉点号.  ，这里记得 后面的截取 s字符串，也要从 i+2 开始，毕竟有之前的点号 .
+
+            }
+
 
         }
-        // 必须是返回 left。只需要改动这个地方！！！具体原因如下 ：
-        return left;
-        /***
-         *  以上while循环中，若找到了target直接返回
 
-         *  当原数组不包含target时，考虑while循环最后一次执行的总是 left=right=mid,
+    }
 
-         *  此时nums[mid] 左边的数全部小于target，nums[mid]右边的数全部大于target,
 
-         *  则此时我们要返回的插入位置  分为两种情况：
+    // 判断字符串 s 在  左闭右闭区间[start, end] 所组成的数字是否合法
+    public static boolean isValid(String s, int start, int end) {
 
-         *  ①是该位置的右边一个，即nums[mid]<target时，此时执行了left=mid+1,返回left也正确
-         *  ②就是这个位置，即nums[mid]>target时，此时执行了right=mid-1，返回left正确
+        if (start > end) {
+            return false;
+        }
 
-         ***/
+        if (s.charAt(start) == '0' && start!= end) { // 1、开头是0 的数字不合法
+            return false;
+        }
+
+
+        int num = 0;
+        // 因为是闭区间，所以要 <=
+        for (int i = start; i <= end; i++) {
+
+            if (s.charAt(i) > '9' || s.charAt(i) < '0') { // 2、遇到非数字字符不合法
+                return false;
+            }
+            num = num * 10 + (s.charAt(i) - '0');
+            if (num > 255) { // 3、如果大于 255 了不合法
+                return false;
+            }
+
+        }
+
+        return true;
 
     }
 
@@ -162,8 +209,8 @@ public class A  {
         int num=0;
         num ='2'-'0';
         System.out.println(num);
-```````
-        searchInsert(new int[]{1,3,5,6},2);
+
+        restoreIpAddresses("25525511135");
 
     }
 }
