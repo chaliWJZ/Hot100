b22b5f155448634c50a8d8d9fae17905fb556b9a
CharlieJ <zwj_0122@163.com>
2026-08-19 01:14:21 +0800

    34 在排序数组中查找元素的第一个和最后一个位置

--- LeetCodeReview/src/main/java/org/example/CommonArray/No34/Solution34.java
+++ LeetCodeReview/src/main/java/org/example/CommonArray/No34/Solution34.java
@@ -3,11 +3,14 @@ package org.example.CommonArray.No34;
 import java.util.*;
 public class Solution34 {
 
-    // --- 解法一 ---
-// 主要思想 ：  相比于之前的 二分查找(真正的递增有序)，本题目是 非递减，其实也就是类似 递增“有序”，也就是里面的一些  元素会重复！！如果此时我们的 nums[mid] = target ,但是我们不能确定 mid 是否为该目标数的左边界，所以此时我们不可以返回下标。所以就是要把判断  == 和 < 一起合并，继续在左半区间 查询。 == 和 > 的也类似 。。。。
+    // --- 解法一 ：二分查找---
+// 主要思想 ：  相比于之前的 二分查找(真正的递增有序)，本题目是 非递减，其实也就是类似 递增“有序”，也就是里面的一些  元素会重复！！
+// 如果此时我们的 nums[mid] = target ,但是我们不能确定 mid 是否为该目标数的左边界，所以此时我们 不可以 直接返回 mid下标 return mid
+//而是要把if判断 target 和 nums[mid]  == 和 < 一起合并，继续移动 右指针right，在左半区间 查询。它们 公用一行代码 right = mid -1
+// 同理，找target目标值的 上边界，就是要把 if判断 target 和 nums[mid]  == 和 > 一起合并，在右半区间 查询。它们 公用一行代码 left = mid + 1
 
-// 定义两个方法，也就是进行 两次 “二分查找”，，，分别找 下边界 和上边界 。。。。
 
+// 定义两个方法，也就是进行 两次 “二分查找”，，，分别找 下边界low 和上边界 。。。。
     public int[] searchRange (int[] nums, int target) {
 				
          int low = lowerBound(nums,target);  	// 找 下边界 的方法 
@@ -18,7 +21,8 @@ public class Solution34 {
              return new int[]{-1,-1};
          }
 			
-     //  其他情况的话， 如果 ==，那么就只有一个元素， > 就说明 target存在多个，有上下边界
+     //  其他情况的话，  upper > low, 就说明 target目标值存在多个，有上下边界 如果 ==，那么就只有一个元素。
+        // 如果 ==，那么就只有一个元素。
          return new int[]{low,upper};
     }
 
@@ -31,10 +35,12 @@ public class Solution34 {
             
             int mid = (left + right)/2;
 
-            if (target <= nums[mid]) 
-                
-                   // 小于的时候，此时要 带上 = 
-                //当目标值 小于等于 nums[mid]时，继续在左区间查询
+            // 这里就少掉了 target == nums[mid]的 if判断，不需要直接 return mid下标了，需要继续向左⬅️寻找 下边界
+            // 只剩下 if 、 else 两种情况了
+
+            if (target <= nums[mid])
+                   // target 小于 nums[mid]的时候，此时判断条件 还要 带上 =
+                //当目标值target 小于等于 nums[mid]时，继续在左区间查询
                 right = mid - 1;
 
             else if (target > nums[mid]) 
@@ -48,7 +54,7 @@ public class Solution34 {
         
     }
 
-    //  计算上边界， 和上面的求 上边界， 里面的代码 就是相反的改动 
+    //  计算上边界， 和上面的求 上边界， 里面的代码 就是“相反”的改动
     int upperBound(int[] nums, int target) {
         
         int left = 0, right = nums.length - 1;
@@ -56,10 +62,12 @@ public class Solution34 {
         while (left <= right) {    
             
             int mid = (left + right)/2;
-            
-            		      //  大于的时候，此时要 带上 = 
-            		//当target目标值 大于等于 nums[mid]时，继续在右区间查询
-            if (target >= nums[mid]) 
+            // 这里就少掉了 target == nums[mid]的 if判断，不需要直接 return mid下标了，需要继续向右➡️寻找 上边界
+
+
+            if (target >= nums[mid])
+                //  大于的时候，此时要 带上 =
+                //当target目标值 大于等于 nums[mid]时，继续在右区间查询
                  left = mid + 1;               
             else if (target < nums[mid]) 
                 right = mid - 1;
--- LeetCodeReview/src/main/java/org/example/CommonArray/No34/Test.java
+++ LeetCodeReview/src/main/java/org/example/CommonArray/No34/Test.java
@@ -1,11 +1,22 @@
 package org.example.CommonArray.No34;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
         Solution34 solution = new Solution34();
-        // nums = [5,7,7,8,8,10], target = 8
-        System.out.println("Test");
+        int[] nums = {5, 7, 8, 8, 10};
+
+        // 用例1: 找得到（重复元素，返回第一个和最后一个位置）
+        System.out.println("用例1: nums=[5,7,8,8,10], target=8");
+        System.out.println("  结果: " + Arrays.toString(solution.searchRange(nums, 8)) + " (期望: [2, 3])");
+
+        // 用例2: 找不到（返回 [-1,-1]）
+        System.out.println("\n用例2: nums=[5,7,8,8,10], target=6");
+        System.out.println("  结果: " + Arrays.toString(solution.searchRange(nums, 6)) + " (期望: [-1, -1])");
+
+        // 用例3: 单个元素，上下边界相同（target=7 只出现一次，low == upper）
+        System.out.println("\n用例3: nums=[5,7,8,8,10], target=7");
+        System.out.println("  结果: " + Arrays.toString(solution.searchRange(nums, 7)) + " (期望: [1, 1])");
     }
 }
